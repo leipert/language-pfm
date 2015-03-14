d8690eaef294da1e75008332d56a998c91e2ea20
Lukas Eipert <git@leipert.io>
2015-03-14 12:12:22 +0100

    Added automatic deployment.

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-gulp
-cp -rf src/README.md README.md
-rm -rf {grammars,settings,snippets}/gfm.cson script src gulpfile.js clean.sh
-
-#Merge Conflicts
-#apm publish minor
-#git checkout master
